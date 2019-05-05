481623dafcf9134c68b3a739b129b53913627e5c
Jesus de Gregorio <31138976+jdegre@users.noreply.github.com>
2019-05-05 04:23:04 +0200

    Delete build.js

--- build.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-const SwaggerParser = require('swagger-parser');
-fs.readdir(".", function (err, files) {
-    files.forEach(function (file) {
-        if (file.indexOf(".yaml") > 0) SwaggerParser.validate(file);
-    });
-});
