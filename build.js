d9f202105dc572659624b12409d6beb969873100
Jesus de Gregorio <31138976+jdegre@users.noreply.github.com>
2019-05-05 04:20:54 +0200

    Update build.js

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const fs = require('fs');
-var SwaggerParser = require('swagger-parser');
+const SwaggerParser = require('swagger-parser');
 fs.readdir(".", function (err, files) {
     files.forEach(function (file) {
-        if (file.indexOf(".yaml") > 0) SwaggerParser().validate(file);
+        if (file.indexOf(".yaml") > 0) SwaggerParser.validate(file);
     });
 });
